0890d75a41f37133da74d024bfed5a0a4cac901c
SStive39 <sstive39@gmail.com>
2020-08-06 20:10:02 +0300

    version 1.0

--- Web/web.py
+++ Web/web.py
@@ -1,19 +1,40 @@
-from flask import Flask, request, jsonify
+import json
+import os
+
+from flask import Flask, request, send_file
+from zipfile import ZipFile
+import utils
 
 app = Flask(__name__)
 
 
-@app.route('/upd/check', methods=['GET', 'POST'])
-def upd_check():
-    data = request.get_json()
-    # TODO: handle data
-    return jsonify({"delete": ["file1", "file2"], "download": ["file1", "file2"]})
+@app.route('/mods/upd/get', methods=['GET', 'POST'])
+def upd_get():
+    client_mods = json.loads(request.get_json())
+    server_mods = utils.check_mods('mods')
 
+    if not client_mods:
+        client_mods = {}
 
-@app.route('/upd/get/<mod>')
-def upd_get(mod):
-    # TODO: return mod file
-    return 'Update get ' + mod + '!'
+    data = utils.get_updates(client_mods, server_mods)
+
+    # Creating archive
+    if os.path.exists('tmp/mods.zip'):
+        os.remove('tmp/mods.zip')
+    if os.path.exists('tmp/delete_mods.tmp'):
+        os.remove('tmp/delete_mods.tmp')
+
+    del_mods = open('tmp/delete_mods.tmp', 'w')
+    del_mods.write('\n'.join(data['delete']))
+    del_mods.close()
+
+    mods = ZipFile('tmp/mods.zip', 'w')
+    mods.write('tmp/delete_mods.tmp', 'delete_mods.tmp')
+    for mod in data['update']:
+        mods.write(f'mods/{mod}', mod)
+    mods.close()
+
+    return send_file('tmp/mods.zip', attachment_filename='mods_temp.zip')
 
 
 if __name__ == '__main__':
--- lang.py
+++ lang.py
@@ -1,14 +1,29 @@
 en = {
     'greeting':             "Mods updater v%v% by SStive39",
-    'path.wrong':           "Wrong path: %path%",
+    'path.wrong':           "Cant find minecraft in: %path%",
     'path.enter':           "Please enter right path:",
     'step.scan':            "Scanning mods on client",
-    'step.upd.check':       "Checking for updates",
+    'step.upd.get':         "Updating",
     'step.upd.delete':      "Deleting old mods",
-    'step.upd.download':    "Downloading updates",
     'ok':                   "Ok",
     'done':                 "Done!",
-    'error.server':         "Server error! Please check internet connection or contact with administrator."
+    'error.server':         "Server error! Please check internet connection or contact with developer.",
+    'error.download'        "Downloading error! Please make sure that path %path% is writeable or run script as administrator."
+    'os.unsupported':       "Sorry, your os is not supported"
+}
+
+rus = {
+    'greeting':             "Mods updater v%v% by SStive39",
+    'path.wrong':           "Не удается найти Minecraft в папке: %path%",
+    'path.enter':           "Пожалуйста, введите путь к папке .minecraft:",
+    'step.scan':            "Сканирование установленных модов",
+    'step.upd.get':         "Обновление модов",
+    'step.upd.delete':      "Удаление старых версий модов",
+    'ok':                   "Ок",
+    'done':                 "Готово!",
+    'error.server':         "Ошибка сервера! Пожалуйста, проверьте интернет соединение или свяжитесь с разработчиком.",
+    'error.download'        "Ошибка скачивания! Убедитесь что путь %path% Доступен для записи или запустите программу от имени администратора."
+    'os.unsupported':       "К сожалению, эта программа не работает на вашей операционной системе :("
 }
 
 
@@ -21,8 +36,8 @@ def p(key, **vals):
         print(phrase + '\n')
 
 
-def get(key, **vals):
-    phrase = en[key]
-    for k, v in vals.items():
+def get(key, **values):
+    phrase = rus[key]
+    for k, v in values.items():
         phrase = phrase.replace(f"%{k}%", v)
     return phrase
